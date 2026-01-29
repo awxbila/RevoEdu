549db0c9196300ce3546ad7bd235448fb33295b1
Nabilah Nur Hazimah <nablahnur54@gmail.com>
2026-01-29 10:30:26 +0700

    uptodate

--- lms-frontend/app/api/auth/users/route.ts
+++ lms-frontend/app/api/auth/users/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import users from "./users.json";
+import users from "../users.json";
 
 export async function GET(request: NextRequest) {
   // Get token from request header
